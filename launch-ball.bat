064a8ab5c4db7a83d6f311918ac3b8b3cf180a75
oryshchyn <oryshchyn@users.noreply.github.com>
2016-11-04 01:57:25 +0100

    Initial commit

---
+++ launch-ball.bat
@@ -0,0 +1,2 @@
+@echo OFF
+dosbox -fullscreen -noconsole -exit -c "@echo OFF" -c "mount C: %CD%" -c "C:\Ball\Ball.exe" -c "exit"
---
+++ launch-electron.bat
@@ -0,0 +1,2 @@
+@echo OFF
+dosbox -fullscreen -noconsole -exit -c "@echo OFF" -c "mount C: %CD%" -c "C:\Demo\Electron.exe" -c "exit"

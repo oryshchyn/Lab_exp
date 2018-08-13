cb106d904db11a7b773b520e8d8587207a689d2d
oryshchyn <oryshchyn@users.noreply.github.com>
2018-08-13 16:42:39 +0200

    Added auto-detection of DOSBox to Windows scripts. Unfortunately, this restricts supported DOSBox to version 0.74.

--- launch-ball.bat
+++ launch-ball.bat
@@ -1,3 +1,20 @@
 @echo OFF
-dosbox -fullscreen -noconsole -exit -c "@echo OFF" -c "mount C: %CD%" -c "C:\Ball\Ball.exe" -c "exit"
+setlocal enabledelayedexpansion
+set pathprefix=""
+where "dosbox.exe"
+if errorlevel 1 (
+  set defaultpath="C:\Program Files\DOSBox-0.74"
+  set defaultpath86="C:\Program Files (x86)\DOSBox-0.74"
+  echo %PATH%
+  if exist ""%defaultpath%"" (
+    echo old 
+    set pathprefix=%defaultpath%\
+  )
+  if exist ""%defaultpath86%"" (
+    echo new
+    set pathprefix=%defaultpath86%\
+  )
+  echo !pathprefix!
+)
+!pathprefix!dosbox -fullscreen -noconsole -exit -c "@echo OFF" -c "mount C: %CD%" -c "C:\Ball\Ball.exe" -c "exit"
 
